8e6b887fa7feb20a8e45e42fcb622485ede696cd
Mohi Uddin <mohin562@gmail.com>
2021-11-04 01:04:58 +0600

    Finalize logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IPManagementFrontend';
+  token:any;
+  constructor(private router:Router){}
+
+  ngOnInit(): void {
+    this.token = localStorage.getItem('authToken');
+  }
+
+  logout(){
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('userInfo');
+    this.router.navigate(['login']);
+  }
 }
